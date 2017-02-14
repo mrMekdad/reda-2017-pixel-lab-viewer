7657269aa0c4c617566f89bc6e162febe3c99937
mrMekdad <mekdadr6@gmail.com>
2017-02-14 23:15:00 +0100

    pixel-lab-viewer: add repository metadata and packaging

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="reda-2017-pixel-lab-viewer",
+    version="0.1.0",
+    description="Desktop image inspection utilities for microscopy-like frames and manual annotation drills.",
+    author="Red@",
+    packages=["pixel_lab_viewer"],
+    package_dir={"": "src"},
+)
